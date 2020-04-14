27a5c8e895aa8a740aa80c1114cc7072baf02d0f
Kamy <kamy.leeret@gmail.com>
2020-04-13 23:36:59 -0600

    psuedocode

---
+++ bamazonCustomer.js
@@ -0,0 +1,27 @@
+
+
+
+//display all of the items available for sale. 
+// ID | PRODUCT  NAME | PRICE | STOCK ON HAND
+
+//prompt 
+
+// Q-1 What is the product ID of the item you want to buy?
+
+// Q-2 How many units would you like to buy?
+
+///customer places order////
+
+
+///check inventory 
+
+
+//INSUFFICENT QTY IF --> 
+    // 0 STOCK ON HAND  --> "INSIFFICIENT QUANITITY"  
+            PROMPT (WOULD YOU LIKE TO PICK ANOTHER ITEM?)
+            //YES --> 
+                PROMPT (Q1 and Q2)
+
+    //  ELSE (CURRENT STOCK ON HAND - UNITS OF PURCHASE) > 0  
+    //  ELSE CURRENT STOCK ON HAND - UNITS OF PURCHASE 
+
