e4b59905fd3272d48075a76b60a0fc1291e14fd4
Kamy <kamy.leeret@gmail.com>
2020-04-13 23:50:35 -0600

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,11 +17,56 @@
 
 
 //INSUFFICENT QTY IF --> 
+
+var orderQty = 0
+var sohQty = 0
+
     // 0 STOCK ON HAND  --> "INSIFFICIENT QUANITITY"  
             PROMPT (WOULD YOU LIKE TO PICK ANOTHER ITEM?)
-            //YES --> 
+            //IF YES --> 
                 PROMPT (Q1 and Q2)
+            //IF ELSE --> (UNITS OF PURCHASE - CURRENT STOCK ON HAND) = X   
+                
+                this.check 
 
-    //  ELSE (CURRENT STOCK ON HAND - UNITS OF PURCHASE) > 0  
-    //  ELSE CURRENT STOCK ON HAND - UNITS OF PURCHASE 
+                    function checkStock(){
 
+                        var x = (orderQty-sohQty)  // the (outcome of order qty - soh qty)
+
+                        if (x > sohQty || sohQty === x) {
+                        
+                            console.log("Your order has been placed")
+                            updateStockQty(); // deduct order qty from stock on hand and update database record with new SOH Qty.
+                            prompt(); // would you like to place another order.
+    
+                        } else if (x < 0) {
+                            console.log("We no longer have that item in stock")
+                            prompt(); // would you like to place another order.
+
+                                yes 
+
+                                if (x = )
+    
+                                    prompt(would you like to purchase 3 instead?)
+
+                                    IF YES -->  
+
+                                        console.log("Your order has been placed")
+                                        prompt(); // would you like to place another order.   
+                                        updateStockQty(); // deduct order qty from stock on hand and update database record with new SOH Qty.
+                
+
+
+                                    UPDATE 
+    
+                            ELSE IF -->
+                                        console.log("We no longer have that item in stock")
+
+                                         updateStockQty(); // deduct order qty from stock on hand and update database record with new SOH Qty.
+
+                                else if {
+
+                                }
+
+                        }
+                             
