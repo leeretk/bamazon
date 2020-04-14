16803a2c2a34240a0e77e08fd83414457505e0b0
Kamy <kamy.leeret@gmail.com>
2020-04-14 08:11:05 -0600

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,9 +10,9 @@ var Mysql = new Mysql(keys.SQL);
 
 console.log(keys.SQL)
 
-//vars to capture input process.argv
-var itemNumber = process.argv[2];
-var orderQty = process.argv.slice(3).join(" ")
+// //vars to capture input process.argv
+// var itemNumber = process.argv[2];
+// var orderQty = process.argv.slice(3).join(" ")
 
 // Run the test and log it to console immediately
 console.log(process.argv[2] === process.argv[3]);
@@ -30,90 +30,97 @@ console.log("I am at point 1!");
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  newSong();
+//   newitem();
 });
 
 
 
-//display all of the items available for sale. 
-// ID | PRODUCT  NAME | PRICE | STOCK ON HAND
+// //display all of the items available for sale. 
+// // ID | PRODUCT  NAME | PRICE | STOCK ON HAND
 
-//prompt 
+// //prompt 
 
-// Q-1 What is the product ID of the item you want to buy?
+// // Q-1 What is the product ID of the item you want to buy?
 
-// Q-2 How many units would you like to buy?
+// // Q-2 How many units would you like to buy?
 
-///customer places order////
+// ///customer places order////
 
-// validateInput makes sure that the user is supplying only positive integers for their inputs
-        function validateInput(value) {
-            var integer = Number.isInteger(parseFloat(value));
-            var sign = Math.sign(value);
+// // validateInput makes sure that the user is supplying only positive integers for their inputs
+//         function validateInput(value) {
+//             var integer = Number.isInteger(parseFloat(value));
+//             var sign = Math.sign(value);
 
-            if (integer && (sign === 1)) {
-                return true;
-            } else {
-                return 'Please enter a whole non-zero number.';
-            }
-        }
-
-
-
-///check inventory 
+//             if (integer && (sign === 1)) {
+//                 return true;
+//             } else {
+//                 return 'Please enter a whole non-zero number.';
+//             }
+//         }
 
 
-//INSUFFICENT QTY IF --> 
 
-var orderQty = 0
-var sohQty = 0
+// ///pick an item
 
-    // // 0 STOCK ON HAND  --> "INSIFFICIENT QUANITITY"  
-    //         PROMPT (WOULD YOU LIKE TO PICK ANOTHER ITEM?)
-    //         //IF YES --> 
-    //             PROMPT (Q1 and Q2)
-            //IF ELSE --> (UNITS OF PURCHASE - CURRENT STOCK ON HAND) = X   
+// function requestItem() {
+
+
+// }
+
+// ///check inventory 
+
+
+// //INSUFFICENT QTY IF --> 
+
+// var orderQty = 0
+// var sohQty = 0
+
+//     // // 0 STOCK ON HAND  --> "INSIFFICIENT QUANITITY"  
+//     //         PROMPT (WOULD YOU LIKE TO PICK ANOTHER ITEM?)
+//     //         //IF YES --> 
+//     //             PROMPT (Q1 and Q2)
+//             //IF ELSE --> (UNITS OF PURCHASE - CURRENT STOCK ON HAND) = X   
                 
-                this.check 
+//                 this.check 
 
-                    function checkStock(){
+//                     function checkStock(){
 
-                        var x = (orderQty-sohQty)  // the (outcome of order qty - soh qty)
+//                         var x = (orderQty-sohQty)  // the (outcome of order qty - soh qty)
 
-                        if (x > sohQty || sohQty === x) {
+//                         if (x > sohQty || sohQty === x) {
                         
-                            console.log("Your order has been placed")
-                            updateStockQty(); // deduct order qty from stock on hand and update database record with new SOH Qty.
-                            prompt(); // would you like to place another order.
+//                             console.log("Your order has been placed")
+//                             updateStockQty(); // deduct order qty from stock on hand and update database record with new SOH Qty.
+//                             prompt(); // would you like to place another order.
     
-                        } else if (x < 0) {
-                            console.log("We no longer have that item in stock")
-                            prompt(); // would you like to place another order.
+//                         } else if (x < 0) {
+//                             console.log("We no longer have that item in stock")
+//                             prompt(); // would you like to place another order.
 
-                                yes 
+//                                 yes 
 
-                                if (x = )
+//                                 if (x = )
     
-                                    prompt(would you like to purchase 3 instead?)
+//                                     prompt(would you like to purchase 3 instead?)
 
-                                    IF YES -->  
+//                                     IF YES -->  
 
-                                        console.log("Your order has been placed")
-                                        prompt(); // would you like to place another order.   
-                                        updateStockQty(); // deduct order qty from stock on hand and update database record with new SOH Qty.
+//                                         console.log("Your order has been placed")
+//                                         prompt(); // would you like to place another order.   
+//                                         updateStockQty(); // deduct order qty from stock on hand and update database record with new SOH Qty.
                 
 
 
-                                    UPDATE 
+//                                     UPDATE 
     
-                            ELSE IF -->
-                                        console.log("We no longer have that item in stock")
+//                             ELSE IF -->
+//                                         console.log("We no longer have that item in stock")
 
-                                         updateStockQty(); // deduct order qty from stock on hand and update database record with new SOH Qty.
+//                                          updateStockQty(); // deduct order qty from stock on hand and update database record with new SOH Qty.
 
-                                else if {
+//                                 else if {
 
-                                }
+//                                 }
 
-                        }
+//                         }
                             
